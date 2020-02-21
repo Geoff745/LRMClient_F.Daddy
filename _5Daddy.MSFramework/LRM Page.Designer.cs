931a3d1ec5d6ebc0049ca4d94c7d691a368fa569
George_Lev <Dressylemming@Gmail.com>
2020-02-21 23:02:44 +0000

    Fail Fix

--- _5Daddy.MSFramework/LRM Page.Designer.cs
+++ _5Daddy.MSFramework/LRM Page.Designer.cs
@@ -58,46 +58,51 @@
             // 
             this.PitchLabel.AutoSize = true;
             this.PitchLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.PitchLabel.Location = new System.Drawing.Point(16, 15);
+            this.PitchLabel.Location = new System.Drawing.Point(11, 10);
+            this.PitchLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.PitchLabel.Name = "PitchLabel";
-            this.PitchLabel.Size = new System.Drawing.Size(66, 29);
+            this.PitchLabel.Size = new System.Drawing.Size(44, 20);
             this.PitchLabel.TabIndex = 0;
             this.PitchLabel.Text = "Pitch";
             // 
             // SpeedBox
             // 
             this.SpeedBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.SpeedBox.Location = new System.Drawing.Point(187, 110);
+            this.SpeedBox.Location = new System.Drawing.Point(125, 71);
+            this.SpeedBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SpeedBox.Name = "SpeedBox";
             this.SpeedBox.ReadOnly = true;
-            this.SpeedBox.Size = new System.Drawing.Size(124, 35);
+            this.SpeedBox.Size = new System.Drawing.Size(84, 26);
             this.SpeedBox.TabIndex = 3;
             // 
             // BankBox
             // 
             this.BankBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.BankBox.Location = new System.Drawing.Point(187, 60);
+            this.BankBox.Location = new System.Drawing.Point(125, 39);
+            this.BankBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BankBox.Name = "BankBox";
             this.BankBox.ReadOnly = true;
-            this.BankBox.Size = new System.Drawing.Size(124, 35);
+            this.BankBox.Size = new System.Drawing.Size(84, 26);
             this.BankBox.TabIndex = 4;
             // 
             // PitchBox
             // 
             this.PitchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.PitchBox.Location = new System.Drawing.Point(187, 9);
+            this.PitchBox.Location = new System.Drawing.Point(125, 6);
+            this.PitchBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PitchBox.Name = "PitchBox";
             this.PitchBox.ReadOnly = true;
-            this.PitchBox.Size = new System.Drawing.Size(124, 35);
+            this.PitchBox.Size = new System.Drawing.Size(84, 26);
             this.PitchBox.TabIndex = 5;
             // 
             // BankLabel
             // 
             this.BankLabel.AutoSize = true;
             this.BankLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.BankLabel.Location = new System.Drawing.Point(15, 66);
+            this.BankLabel.Location = new System.Drawing.Point(10, 43);
+            this.BankLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.BankLabel.Name = "BankLabel";
-            this.BankLabel.Size = new System.Drawing.Size(67, 29);
+            this.BankLabel.Size = new System.Drawing.Size(46, 20);
             this.BankLabel.TabIndex = 6;
             this.BankLabel.Text = "Bank";
             // 
@@ -105,37 +110,41 @@
             // 
             this.SpeedLabel.AutoSize = true;
             this.SpeedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.SpeedLabel.Location = new System.Drawing.Point(15, 116);
+            this.SpeedLabel.Location = new System.Drawing.Point(10, 75);
+            this.SpeedLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.SpeedLabel.Name = "SpeedLabel";
-            this.SpeedLabel.Size = new System.Drawing.Size(85, 29);
+            this.SpeedLabel.Size = new System.Drawing.Size(56, 20);
             this.SpeedLabel.TabIndex = 7;
             this.SpeedLabel.Text = "Speed";
             // 
             // WindSpeedBox
             // 
             this.WindSpeedBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.WindSpeedBox.Location = new System.Drawing.Point(187, 163);
+            this.WindSpeedBox.Location = new System.Drawing.Point(125, 106);
+            this.WindSpeedBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.WindSpeedBox.Name = "WindSpeedBox";
             this.WindSpeedBox.ReadOnly = true;
-            this.WindSpeedBox.Size = new System.Drawing.Size(124, 35);
+            this.WindSpeedBox.Size = new System.Drawing.Size(84, 26);
             this.WindSpeedBox.TabIndex = 9;
             // 
             // WindHeadingBox
             // 
             this.WindHeadingBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.WindHeadingBox.Location = new System.Drawing.Point(187, 214);
+            this.WindHeadingBox.Location = new System.Drawing.Point(125, 139);
+            this.WindHeadingBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.WindHeadingBox.Name = "WindHeadingBox";
             this.WindHeadingBox.ReadOnly = true;
-            this.WindHeadingBox.Size = new System.Drawing.Size(124, 35);
+            this.WindHeadingBox.Size = new System.Drawing.Size(84, 26);
             this.WindHeadingBox.TabIndex = 8;
             // 
             // WindHeadingLabel
             // 
             this.WindHeadingLabel.AutoSize = true;
             this.WindHeadingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.WindHeadingLabel.Location = new System.Drawing.Point(15, 217);
+            this.WindHeadingLabel.Location = new System.Drawing.Point(10, 141);
+            this.WindHeadingLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.WindHeadingLabel.Name = "WindHeadingLabel";
-            this.WindHeadingLabel.Size = new System.Drawing.Size(165, 29);
+            this.WindHeadingLabel.Size = new System.Drawing.Size(109, 20);
             this.WindHeadingLabel.TabIndex = 11;
             this.WindHeadingLabel.Text = "Wind Heading";
             // 
@@ -143,47 +152,52 @@
             // 
             this.WindSpeedLabel.AutoSize = true;
             this.WindSpeedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.WindSpeedLabel.Location = new System.Drawing.Point(15, 169);
+            this.WindSpeedLabel.Location = new System.Drawing.Point(10, 110);
+            this.WindSpeedLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.WindSpeedLabel.Name = "WindSpeedLabel";
-            this.WindSpeedLabel.Size = new System.Drawing.Size(146, 29);
+            this.WindSpeedLabel.Size = new System.Drawing.Size(96, 20);
             this.WindSpeedLabel.TabIndex = 10;
             this.WindSpeedLabel.Text = "Wind Speed";
             // 
             // FPMBox
             // 
             this.FPMBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.FPMBox.Location = new System.Drawing.Point(516, 9);
+            this.FPMBox.Location = new System.Drawing.Point(344, 6);
+            this.FPMBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.FPMBox.Name = "FPMBox";
             this.FPMBox.ReadOnly = true;
-            this.FPMBox.Size = new System.Drawing.Size(189, 64);
+            this.FPMBox.Size = new System.Drawing.Size(127, 45);
             this.FPMBox.TabIndex = 12;
             // 
             // FPMLabel
             // 
             this.FPMLabel.AutoSize = true;
             this.FPMLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FPMLabel.Location = new System.Drawing.Point(376, 15);
+            this.FPMLabel.Location = new System.Drawing.Point(251, 10);
+            this.FPMLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.FPMLabel.Name = "FPMLabel";
-            this.FPMLabel.Size = new System.Drawing.Size(134, 58);
+            this.FPMLabel.Size = new System.Drawing.Size(92, 39);
             this.FPMLabel.TabIndex = 13;
             this.FPMLabel.Text = "FPM";
             // 
             // ScoreBox
             // 
             this.ScoreBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.ScoreBox.Location = new System.Drawing.Point(516, 79);
+            this.ScoreBox.Location = new System.Drawing.Point(344, 51);
+            this.ScoreBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ScoreBox.Name = "ScoreBox";
             this.ScoreBox.ReadOnly = true;
-            this.ScoreBox.Size = new System.Drawing.Size(161, 35);
+            this.ScoreBox.Size = new System.Drawing.Size(109, 26);
             this.ScoreBox.TabIndex = 14;
             // 
             // ScoreLabel
             // 
             this.ScoreLabel.AutoSize = true;
             this.ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.ScoreLabel.Location = new System.Drawing.Point(408, 79);
+            this.ScoreLabel.Location = new System.Drawing.Point(272, 51);
+            this.ScoreLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.ScoreLabel.Name = "ScoreLabel";
-            this.ScoreLabel.Size = new System.Drawing.Size(93, 36);
+            this.ScoreLabel.Size = new System.Drawing.Size(64, 25);
             this.ScoreLabel.TabIndex = 15;
             this.ScoreLabel.Text = "Score";
             // 
@@ -192,9 +206,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(3, 312);
+            this.label2.Location = new System.Drawing.Point(2, 203);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(290, 36);
+            this.label2.Size = new System.Drawing.Size(195, 25);
             this.label2.TabIndex = 18;
             this.label2.Text = "Connected To: None";
             // 
@@ -203,9 +218,10 @@
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(177)))), ((int)(((byte)(89)))));
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(3, 257);
+            this.button1.Location = new System.Drawing.Point(2, 167);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(144, 57);
+            this.button1.Size = new System.Drawing.Size(96, 37);
             this.button1.TabIndex = 19;
             this.button1.Text = "Connect To FlightSim";
             this.button1.UseVisualStyleBackColor = false;
@@ -216,9 +232,10 @@
             this.button2.BackColor = System.Drawing.Color.Red;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
             this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button2.Location = new System.Drawing.Point(149, 257);
+            this.button2.Location = new System.Drawing.Point(99, 167);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(144, 57);
+            this.button2.Size = new System.Drawing.Size(96, 37);
             this.button2.TabIndex = 20;
             this.button2.Text = "Disconnect FlightSim";
             this.button2.UseVisualStyleBackColor = false;
@@ -227,18 +244,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(317, 125);
+            this.label1.Location = new System.Drawing.Point(211, 81);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 21;
             this.label1.Text = "knots";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(317, 178);
+            this.label3.Location = new System.Drawing.Point(211, 116);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.Size = new System.Drawing.Size(33, 13);
             this.label3.TabIndex = 22;
             this.label3.Text = "knots";
             // 
@@ -246,9 +265,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label4.Location = new System.Drawing.Point(316, 60);
+            this.label4.Location = new System.Drawing.Point(211, 39);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(21, 29);
+            this.label4.Size = new System.Drawing.Size(14, 20);
             this.label4.TabIndex = 23;
             this.label4.Text = "°";
             // 
@@ -256,9 +276,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label5.Location = new System.Drawing.Point(316, 9);
+            this.label5.Location = new System.Drawing.Point(211, 6);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(21, 29);
+            this.label5.Size = new System.Drawing.Size(14, 20);
             this.label5.TabIndex = 24;
             this.label5.Text = "°";
             this.label5.Click += new System.EventHandler(this.label5_Click);
@@ -267,16 +288,17 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label6.Location = new System.Drawing.Point(316, 214);
+            this.label6.Location = new System.Drawing.Point(211, 139);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(21, 29);
+            this.label6.Size = new System.Drawing.Size(14, 20);
             this.label6.TabIndex = 25;
             this.label6.Text = "°";
             this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // LRM_Page
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.Controls.Add(this.label6);
@@ -301,8 +323,9 @@
             this.Controls.Add(this.BankBox);
             this.Controls.Add(this.SpeedBox);
             this.Controls.Add(this.PitchLabel);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "LRM_Page";
-            this.Size = new System.Drawing.Size(1062, 356);
+            this.Size = new System.Drawing.Size(708, 231);
             this.Load += new System.EventHandler(this.LRM_Page_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -333,6 +356,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        public System.Windows.Forms.Timer OffsetReaderTimer;
+        private System.Windows.Forms.Timer OffsetReaderTimer;
     }
 }
